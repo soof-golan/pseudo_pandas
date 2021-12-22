eb9315e813a2662a89ccb0804f3db731e3cab6a9
soof-golan <83900570+soof-golan@users.noreply.github.com>
2021-12-22 20:08:16 +0200

    moar tests!

--- pseudo_pandas/series.py
+++ pseudo_pandas/series.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 
 class LocHandler:
 
@@ -12,8 +12,19 @@ class LocHandler:
 
 class Series:
     def __init__(self, data, index=None):
-        self.data = np.array(data)
+        if isinstance(data, dict):
+            assert index is None
+            index = list(data.keys())
+            data = [data[k] for k in index]
+        if not hasattr(data, '__iter__'):
+            if index is None:
+                raise ValueError('constant series must not have index')
+            data = np.full(len(index), data)
+            print(data)
+        data = np.array(data)
+        self.data = data
         index = index or range(len(self.data))
+        self.sliceable_index = index
         self.index = {k: i for i, k in enumerate(index)}
 
     @property
@@ -28,3 +39,18 @@ class Series:
 
     def __repr__(self):
         return '\n'.join(f'{idx}\t{v}' for idx, v in self) + f'\ndtype:{self.data.dtype}'
+
+    def __getitem__(self, i):
+        if isinstance(i, slice):
+            return Series(self.data[i], self.sliceable_index[i])
+        return self.loc[i]
+
+    def __array_ufunc__(self, ufunc: np.ufunc, method: str, *inputs, **kwargs):
+        data_inputs = [x if x is not self else self.data for x in inputs]
+        result = getattr(ufunc, method)(*data_inputs, **kwargs)
+        if not hasattr(result, "__iter__"):
+            return result
+        return Series(data=result, index=self.index)
+
+    def __eq__(self, other):
+        return all((v == o for v, o in zip(self, other)))
--- test/test_series.py
+++ test/test_series.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 
 import pseudo_pandas as pp
 
@@ -25,7 +26,26 @@ class SeriesTestCase(unittest.TestCase):
         my_sr = pp.Series(np.array([1, 2, 3]))
         self.assertEqual(repr(my_sr), """0\t1\n1\t2\n2\t3\ndtype:int32""")
 
+    def test_from_dict(self):
+        s = pp.Series({1: 'a', 2: 'b'})
+        self.assertEqual(s.loc[1], 'a')
 
+    def test_constant(self):
+        s = pp.Series(1, [1, 2, 4])
+        self.assertEqual(s.loc[1], 1)
+        self.assertEqual(s.loc[2], 1)
+        self.assertEqual(s.loc[4], 1)
+
+    def test_slice(self):
+        s = pp.Series(np.arange(3), ['A', 'B', 'C'])
+        self.assertEqual(list(s[1:3]), [('B', 1), ('C', 2)])
+
+    def test_numpy_funcs(self):
+        s = pp.Series(np.arange(3), ['A', 'B', 'C'])
+        self.assertEqual(np.sum(s), np.sum(np.arange(3)))
+        s = pp.Series(np.arange(-5, 3))
+        self.assertEqual(np.sign(s), np.sign(np.arange(-5, 3)))
 
 if __name__ == '__main__':
     unittest.main()
+pd.Series
